0fe2f2dcb935803fa6071c65f9cdb85c1c8f413e
Anilowa <ania54345@gmail.com>
2025-03-10 23:20:44 +0100

    Update 2

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DebugCamRot : MonoBehaviour
+public class CamControl : MonoBehaviour
 {
+	public float moveSpeed = 6f;
     public float mouseSensitivity = 72f;
-
     private float xRotation = 0f;
 
     // Start is called before the first frame update
@@ -29,5 +29,27 @@ public class DebugCamRot : MonoBehaviour
 
         // Rotate the camera around the Y-axis
         transform.Rotate(Vector3.up * mouseX, Space.World);
+
+		float leftRight = InputSystem.actions.FindAction("Move").ReadValue<Vector2>().x;
+		float   forBack = InputSystem.actions.FindAction("Move").ReadValue<Vector2>().y;
+		float    upDown = InputSystem.actions.FindAction("Jump").ReadValue<float>();
+
+		Vector3 flatMove = moveSpeed * Time.deltaTime * new Vector3(leftRight, upDown, forBack);
+        transform.Translate(flatMove);
     }
+
+	void OnGUI()
+	{
+		// Set the style for the label
+		GUIStyle style = new()
+		{
+			fontSize = 24
+		};
+		style.normal.textColor = Color.white;
+
+		// Draw the FPS label in the lower-left corner
+		GUI.Label(new Rect(10, 10, 100, 25), "text", style);
+		// Draw debug label below
+		// GUI.Label(new Rect(10, 40, 100, 25), 216.ToString(), style);
+	}
 }
